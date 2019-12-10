5949c75eb642bf43c3b79edfa4eb48ec2db4d4cc
Zac Higgins <zachiggins@Higgins-iMac.local>
2019-12-10 11:57:54 -0700

    added fruits table and remade database

--- data/migrations/20191210104308_addVeggiesTable.js
+++ data/migrations/20191210104308_addVeggiesTable.js
@@ -1,8 +1,17 @@
 
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+    return knex.schema.createTable('veggies', tbl => {
+        tbl.increments();
+
+        tbl.string('name', 255)
+            .notNullable()
+            .unique()
+            .index();
+
+        tbl.boolean('like_it').defaultTo(false)
+    })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('veggies');
 };
--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: './data/produce.db3'
     },
     migrations: {
       directory: './data/migrations'
